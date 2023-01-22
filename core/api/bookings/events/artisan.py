650649ad8705920c697ad11dff08a65b33e731c7
curiouspaul1 <170408025@live.unilag.edu.ng>
2023-01-22 22:23:14 +0100

    debugging'

--- core/api/bookings/events/artisan.py
+++ core/api/bookings/events/artisan.py
@@ -53,6 +53,9 @@ def connect():
 
 @socketio.on('location_update', namespace='/artisan')
 def update_location(data):
+    data = data.replace('\\n', '')
+    data = eval(data)
+    print(data, type(data))
     # TODO: add data validation
     # update artisan location on redis
     room = data['artisan_id']
@@ -85,6 +88,9 @@ def update_location(data):
 
 @socketio.on('accept_offer', namespace='/artisan')
 def get_updates(data):
+    data = data.replace('\\n', '')
+    data = eval(data)
+    print(data, type(data))
     """ triggered when artisan accepts offer """
     from tasks.booking_tasks import assign_artisan_to_booking
 
