020737e941b4e129e73ea028ac0113c32e3c12ae
curiouspaul1 <170408025@live.unilag.edu.ng>
2023-05-24 17:21:27 +0100

    updates

--- schemas/user_schemas.py
+++ schemas/user_schemas.py
@@ -54,7 +54,11 @@ class UserSchema(BaseSQLAlchemyAutoSchema):
             'created_at'
         )
     artisan_profile = ma.Nested(Artisan(
-        exclude=(
-            'booking'
+        only=(
+            'artisan_id', 'is_verified',
+            'jobs_title', 'jobs_completed',
+            'sign_up_date', 'hourly_rate',
+            'ratings', 'user_id',
+            'job_category_id',
         )
     ), dump_only=True)
