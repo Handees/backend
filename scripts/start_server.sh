3925db07d1e2723b447ef1676c01b58c5c1c8a46
curiouspaul1 <170408025@live.unilag.edu.ng>
2023-06-03 19:04:05 +0100

    updates to pipeline

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-source /tmp/APP_ENV.env
-
-curr_env = $APP_ENV
-
 cd /home/handeesofficial/backend
 source env/bin/activate
 
@@ -14,4 +10,5 @@ flask load-config-variables
 sudo service start nginx
 
 # start workers
-sudo supervisord -c supervisord-$curr_env.conf
+source /tmp/APP_ENV.env
+sudo supervisord -c supervisord-$APP_ENV.conf
