d922133c8ac0e8690c5cc8ec8406527a2cb66174
curiouspaul1 <170408025@live.unilag.edu.ng>
2023-06-03 17:51:14 +0100

    bugsss oo

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
 # set env vars
-echo "ehi"
+echo $APP_ENV
+
+sudo apt-get -y update
+sudo apt-get -y install python3 python3-venv python3-dev
+sudo apt-get -y install supervisor nginx git
+
+# set up virtual environment
+cd /home/handeesofficial/backend
+python3 -m venv env
+source env/bin/activate
+
+# install dependencies
+pip install -r requirements.txt
+pip install gunicorn[eventlet]
