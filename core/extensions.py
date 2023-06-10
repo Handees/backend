ed05b6498673a02018f30b843286fe8eab65fe68
curiouspaul1 <170408025@live.unilag.edu.ng>
2023-06-10 19:35:19 +0100

    updates for redis config

--- core/extensions.py
+++ core/extensions.py
@@ -5,22 +5,21 @@ from flask_socketio import SocketIO
 from flask_cors import CORS
 from flask_session import Session
 # from core.sqlalchemy_ext import RouteSQLAlchemy
-from flask import current_app
+import os
 
 
 db = SQLAlchemy()
 ma = Marshmallow()
-with current_app.app_context():
-    socketio = SocketIO(
-        cors_allowed_origins=[
-            'http://127.0.0.1:5020', 'http://127.0.0.1:5500',
-            'https://www.piesocket.com'
-        ],
-        async_mode='eventlet',
-        message_queue=f"redis://localhost:{current_app.config['REDIS_PORT']}/2",
-        logger=True,
-        engineio_logger=True
-    )
+socketio = SocketIO(
+    cors_allowed_origins=[
+        'http://127.0.0.1:5020', 'http://127.0.0.1:5500',
+        'https://www.piesocket.com'
+    ],
+    async_mode='eventlet',
+    message_queue=f"redis://localhost:{os.getenv('REDIS_PORT', 6378)}/2",
+    logger=True,
+    engineio_logger=True
+)
 sess = Session()
 migrate = Migrate(include_schemas=True)
 cors = CORS()
