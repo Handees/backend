adcf57224a41c9fb17519feb3388d71882bfc403
curiouspaul1 <170408025@live.unilag.edu.ng>
2023-06-19 19:19:11 +0100

    bug fix

--- core/api/bookings/events/utils.py
+++ core/api/bookings/events/utils.py
@@ -53,7 +53,7 @@ def parse_event_data(fn):
             data, *other_args = args
             data = json.loads(data)
             data = eval(data)
-            print(data)
+            del data['local']
             return fn(data, *other_args)
     return decorated
 
