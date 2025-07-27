d5fbd8e0555374f75250840e148d4f91b756f8e7
curiouspaul1 <170408025@live.unilag.edu.ng>
2025-07-27 22:12:39 +0100

    updates

--- redis/redis.sh
+++ redis/redis.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-
 docker run --name redis -d -p 6379:6379/tcp \
     --restart unless-stopped \
-    -v %cd%/redis:/usr/local/etc/redis \
+    -v "$(pwd)/redis:/usr/local/etc/redis" \
     redis redis-server /usr/local/etc/redis/redis.conf
-
